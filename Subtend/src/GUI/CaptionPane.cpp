7b0a84876ea9b2fa8a2dc1491b5c9035c69b2529
Matthew Noyes <manoyes@users.noreply.github.com>
2022-02-28 21:37:45 -0600

    :construction: Add basic caption GUI
    Ref #3

--- Subtend/src/GUI/CaptionPane.cpp
+++ Subtend/src/GUI/CaptionPane.cpp
@@ -9,9 +9,31 @@ namespace Subtend
     void Subtend::CaptionPane::RenderUI()
     {
         ImGui::Begin("Caption Editor");
-        ImGui::Button("Hello");
-        static float value = 0.0f;
-        ImGui::DragFloat("Value", &value);
+
+        // Note: we are using a fixed-sized buffer for simplicity here. See ImGuiInputTextFlags_CallbackResize
+        // and the code in misc/cpp/imgui_stdlib.h for how to setup InputText() for dynamically resizing strings.
+        static char subtitle[50 * 2] =
+            " In the name of the father, and of the son, \n"
+            " En el nombre del padre, del hijo,\n";
+        static char starttime[12] = "00:00:00000";
+        static char endtime[12] = "00:01:00000";
+
+        static ImGuiInputTextFlags flags = ImGuiInputTextFlags_AllowTabInput | ImGuiInputTextFlags_CtrlEnterForNewLine;
+        ImGui::InputTextMultiline("##subtitle", subtitle, IM_ARRAYSIZE(subtitle), ImVec2(ImGui::GetTextLineHeight() * 30, ImGui::GetTextLineHeight() * 3), flags);
+
+        ImGui::SameLine();
+
+        ImGui::Button("X", ImVec2(32, ImGui::GetTextLineHeight() * 3));
+
+        ImGui::SameLine();
+
+        ImGui::BeginGroup();
+        ImGui::InputText("##starttime", starttime, IM_ARRAYSIZE(starttime), flags);
+        ImGui::InputText("##endtime", endtime, IM_ARRAYSIZE(endtime), flags);
+        ImGui::EndGroup();
+
+        ImGui::Button("+", ImVec2(24,24));
+
         ImGui::End();
     }
 
