18a8a8df20f323c91afb5537c4feb939868a4064
Matthew Noyes <manoyes@users.noreply.github.com>
2022-02-28 20:00:39 -0600

    :art: Make convenience functions for smart ptrs
    Ref #3

---
+++ Subtend/src/Core.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <memory>
+
+namespace Subtend
+{
+    template<typename T>
+    using SharedPtr = std::shared_ptr<T>;
+
+    template<typename T>
+    using UniquePtr = std::unique_ptr<T>;
+
+    template <typename T, typename ... Args>
+    constexpr SharedPtr<T> MakeSharedPtr(Args&& ... args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+
+    template <typename T, typename ... Args>    
+    constexpr UniquePtr<T> MakeUniquePtr(Args&& ... args)
+    {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
+}
